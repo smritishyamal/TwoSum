9bf9ba2f624af2a491fb276b9c3ee9dde0f42b26
Smriti Shyamal <smritishyamal123@gmail.com>
2018-01-28 00:19:26 -0500

    Exception handling.
    If no sum found then return the original vector.

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -7,7 +7,9 @@ public:
   vector<int> twoSum(vector<int>& nums, int& target)
   {
     vector<int> g1;
+    unsigned count = 0;
     for (unsigned i = 0; i < nums.size(); i++) {
+      count = count + 1;
         for (unsigned j = i + 1; j < nums.size(); j++) {
             if (nums.at(j) == target - nums.at(i)) {
                 g1.push_back(i);
@@ -16,17 +18,36 @@ public:
             }
         }
     }
-}
+    if (count == nums.size()){
+      cout << "No sum found " << endl;
+      return nums;
+    }
+  }
 } sol;
 
 int main() 
 {
   vector<int> a1 = {2, 7, 11, 15};
   int b1 = 18;
+  int out;
+  
   // Call the function 
   vector<int> p = sol.twoSum(a1,b1);
-  for (unsigned i = 0; i<p.size(); i++){
-     cout << p.at(i) << endl;
-  }  
+  
+  //Check the returned vector
+  a1 == p ? out = 0 : out = 1;
+  
+  //If orginial is returned then no sum found
+  if (out == 0){
+    cout << "no sum found" <<endl ;
+  }
+  
+  // Else print the indices
+  else {
+    for (unsigned i = 0; i<p.size(); i++){
+      cout << p.at(i) << endl;
+    }
+  }
+  
   return 0;
 }
